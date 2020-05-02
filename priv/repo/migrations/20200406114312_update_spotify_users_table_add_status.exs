749f4cb6a2c39c59d2cd527c94c087eac2fd024d
Mike Hoyle <mike.hoyle@centro.net>
2020-05-02 16:43:46 -0500

    Gigalixir - Attempting to get build running. Updated config

--- priv/repo/migrations/20200406114312_update_spotify_users_table_add_status.exs
+++ priv/repo/migrations/20200406114312_update_spotify_users_table_add_status.exs
@@ -2,9 +2,15 @@ defmodule SpotOn.Repo.Migrations.UpdateSpotifyUsersTable do
   use Ecto.Migration
   alias SpotOn.Model.UserStatusEnum
 
-  def change do
+  def up do
     UserStatusEnum.create_type()
+  end
 
+  def down do
+    UserStatusEnum.drop_type()
+  end
+
+  def change do
     alter table(:spotify_users) do
       add :status, UserStatusEnum.type(), null: false, default: "active"
     end
